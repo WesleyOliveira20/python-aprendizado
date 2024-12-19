863535082ebf62ebf6d0c03ba50373f5826bec2d
WesleyOliveira20 <wesleyoliveirasantana618@gmail.com>
2024-12-18 22:50:15 -0300

    Primeira versão do calculo de um Fatorial

--- metodo_5QS.py
+++ metodo_5QS.py
@@ -1,17 +1,3 @@
-# Pseudocódigo sintaxe
-
-"""
-input: palavra usada para receber dados do usuario
-print:exibir o resultado na console
-if condição: condicional que controla se algo deve ou não ser feito
-else: clausula a ser executada  caso nenhuma condicional if seja executada 
-loop de x em y:laço de repetição que ira de X A Y 
-Loop x em Y:laço de repetição que ira iterar X em uma coleção Y
-while: laço repetição que acontecera enquanto uma CONDIÇÃO for verdadeira
-
-"""
-
-
 """
 1 Quais os dados de entrada necessarios ?
 2 oque devo fazer com esses dados?
--- pseudo4.py
+++ pseudo4.py
@@ -1,8 +1,15 @@
-# FATORIAL DE UM NUMÉRO 
+# Programa para calcular o fatorial de um número
+# Exemplo: 5! = 5 x 4 x 3 x 2 x 1 = 120
 
-#pede um número para o usuário
-numero = int(input("Digite um Número para calcular o fatorial: "))
+# Pede o número para o usuário
+numero = int(input('Digite um número para calcular o fatorial: '))
 
-# Inicializa o fatorial como 1 fatorial = 1
+# Inicializa o fatorial como 1
+fatorial = 1
 
+# Calcula o fatorial
+for i in range(1, numero + 1):
+    fatorial = fatorial * i
 
+# Mostra o resultado
+print(f'O fatorial de {numero} é: {fatorial}')
