18ad49c5f9de74109589e1b9b9e8ce0ce00d3083
WesleyOliveira20 <wesleyoliveirasantana618@gmail.com>
2024-12-18 22:31:09 -0300

    Metodo 5Qs,para construção de programas

--- metodo_5QS.py
+++ metodo_5QS.py
@@ -13,6 +13,11 @@ while: laço repetição que acontecera enquanto uma CONDIÇÃO for verdadeira
 
 
 """
-Salario_mensal/horas_trabalhadas
+1 Quais os dados de entrada necessarios ?
+2 oque devo fazer com esses dados?
+3 Quais são s restrições deste problrema?
+4 qual o resultado esperado ?
+5 Qual é a sequencia de passos a ser feitas para chrgar ao resultado esperado(faça isso em pseud codigo)
+
 
 """
--- pseudo4.py
+++ pseudo4.py
@@ -1,1 +1,8 @@
-#iniciando nov pseudo
+# FATORIAL DE UM NUMÉRO 
+
+#pede um número para o usuário
+numero = int(input("Digite um Número para calcular o fatorial: "))
+
+# Inicializa o fatorial como 1 fatorial = 1
+
+
