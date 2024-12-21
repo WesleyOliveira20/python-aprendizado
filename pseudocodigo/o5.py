5715deb662e36b7ba684ac8f0feeac15f7224d11
WesleyOliveira20 <wesleyoliveirasantana618@gmail.com>
2024-12-21 20:22:11 -0300

    Conceitos Basicos de Python

---
+++ pseudocodigo/o5.py
@@ -0,0 +1,14 @@
+#1 Quais os dados de entrada necessarios ?
+"um valor aleatorio de 1 a 10"
+"um valor de chute do usuario"
+
+#2 oque devo fazer com esses dados?
+
+
+#3 Quais são s restrições deste problrema?
+
+
+#4 qual o resultado esperado ?
+
+
+#5 Qual é a sequencia de passos a ser feitas para chrgar ao resultado esperado(faça isso em pseud codigo)
---
+++ python_essencial/pricod.py
@@ -0,0 +1,9 @@
+# Conceitos inicias em python 
+
+# Como criar e usar variáveis em python
+# Como criar e usar condicionais em python
+# Como criar e usar laçso de repetição em python
+# como criar e usar listas em python
+# Como criar e usar funções ( nova versão 2.0 )
+# Como criar e usar classes ( nova versão 2.0)
+
