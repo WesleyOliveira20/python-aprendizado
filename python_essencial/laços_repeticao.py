eb112c3dd8df17f02caa6e65708bf94a22748fe7
WesleyOliveira20 <wesleyoliveirasantana618@gmail.com>
2024-12-31 20:19:28 -0300

    loops

---
+++ python_essencial/laços_repeticao.py
@@ -0,0 +1,28 @@
+# laços de repetição
+'''
+for palavra in range(1,101):
+    print(palavra)
+
+for ITEM in coleção:
+   espressão
+
+for item in range(1,32):
+    print(item)
+
+for item in range (1,33,3):
+    print(item)
+
+nome = ['jhonatan','wesley','vinicius','otavio']
+for nome in nome:
+    print(nome)
+    '''
+'''
+numero_maximo = int(input('Digite um numero:'))
+for numero_maximo in range(1,numero_maximo + 1):
+    print(numero_maximo)
+'''
+
+valor_maximo = int(input('Digite um numero:'))
+valor_inicial = 1
+for numero in range(valor_inicial,valor_maximo + 1):
+    print(numero)
