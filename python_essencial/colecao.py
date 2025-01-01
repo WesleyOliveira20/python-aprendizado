4e214b69f3c2654293564f03ab3bf1d6e598040e
WesleyOliveira20 <wesleyoliveirasantana618@gmail.com>
2025-01-01 18:29:01 -0300

    laços de repetião

---
+++ python_essencial/colecao.py
@@ -0,0 +1,26 @@
+# COLEÇÕES (LISTAS)
+'''
+Preco1 = 20
+preco2 = 100 
+preco3 = 300
+
+precos = [20,100,300]
+#          0   1   2
+print(precos[2])
+print(precos.index(300))
+#lista
+diversidades = [15, 'wesley',True]
+print(diversidades[0])
+print(diversidades[1])
+print(diversidades[2])
+# laços em iteraveis 
+for preco in precos:
+    print(preco)
+'''
+
+
+idades = [12,36,29,9,90,45]
+total = 0 
+for idade in idades:
+    total = total + idade
+print(total)
